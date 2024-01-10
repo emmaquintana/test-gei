3da30ea0758d771dbbe1f9f96b1453bfbc384e8b
Emmanuel Quintana <eqb2021@gmail.com>
2024-01-09 22:48:16 -0300

    feat: Add ModalText component to the page and display it when user press 'YES' button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
+"use client"
+
+import { useState } from 'react';
 import styles from './page.module.css';
+import ModalText from '@/Assets/Components/ModalText';
 
 export default function Home() {    
   
+  const [modalIsDisplayed, setModalIsDisplayed] = useState(false);    
+
   return (    
     <section className={styles.section}>
       <span className={`${styles.logo} ${styles.text} ${styles.question}`}>¿🏳️‍🌈?</span>
@@ -9,9 +15,15 @@ export default function Home() {
         ¿Eres gei?
       </h2>
       <div className={styles.buttonsContainer}>
-        <button className={styles.button}>Si 🏳️‍🌈</button>
+        <button className={styles.button} onClick={() => setModalIsDisplayed(!modalIsDisplayed)}>Si 🏳️‍🌈</button>
         <button className={styles.button}>Ño 🤮</button>
       </div>      
+      {modalIsDisplayed && 
+        <ModalText 
+          content='jajaja re trolx sos. sabido era'
+          close={() => setModalIsDisplayed(false)}
+        />
+      }
     </section>
   )
 }
