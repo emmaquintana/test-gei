a745e8fc026acbf82e7a79ab5b5cbc16c06588f9
Emmanuel Quintana <eqb2021@gmail.com>
2024-01-09 22:44:58 -0300

    feat: Create basic layout and styles for page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,17 @@
-export default function Home() {
-  return (
-    <></>
+import styles from './page.module.css';
+
+export default function Home() {    
+  
+  return (    
+    <section className={styles.section}>
+      <span className={`${styles.logo} ${styles.text} ${styles.question}`}>¿🏳️‍🌈?</span>
+      <h2 className={`${styles.text} ${styles.question}`}>
+        ¿Eres gei?
+      </h2>
+      <div className={styles.buttonsContainer}>
+        <button className={styles.button}>Si 🏳️‍🌈</button>
+        <button className={styles.button}>Ño 🤮</button>
+      </div>      
+    </section>
   )
 }
