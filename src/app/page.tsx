bbd81c3f96801f7e721caab01e6024f03cb29f5c
Emmanuel Quintana <eqb2021@gmail.com>
2024-01-10 00:28:03 -0300

    fix: Handle window's resizing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import styles from './page.module.css';
 import ModalText from '@/Assets/Components/ModalText';
 import randomChangePosition from '@/Assets/Util/RandomChangePosition';
@@ -9,6 +9,22 @@ export default function Home() {
 
   const [modalIsDisplayed, setModalIsDisplayed] = useState(false);
   const noBtnRef = useRef<HTMLButtonElement>(null);  
+
+  useEffect(() => {
+    const handleResize = () => {
+      // Verifies and re set button position after window's resize
+      if (noBtnRef.current) {
+        randomChangePosition(noBtnRef);
+      }
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    // event listener clean
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
 
   return (
     <section className={styles.section}>
