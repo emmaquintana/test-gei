d62b66eda9c4a31ac8ac8cebeec6508974e7896b
Emmanuel Quintana <eqb2021@gmail.com>
2024-01-09 23:27:31 -0300

    feat: Add behaviour to 'No' Button
    When the user press or the mouse is over the 'No' button, the button change its position

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,36 @@
 "use client"
 
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import styles from './page.module.css';
 import ModalText from '@/Assets/Components/ModalText';
+import randomChangePosition from '@/Assets/Util/RandomChangePosition';
 
-export default function Home() {    
-  
-  const [modalIsDisplayed, setModalIsDisplayed] = useState(false);    
+export default function Home() {
 
-  return (    
+  const [modalIsDisplayed, setModalIsDisplayed] = useState(false);
+  const noBtnRef = useRef<HTMLButtonElement>(null);  
+
+  return (
     <section className={styles.section}>
       <span className={`${styles.logo} ${styles.text} ${styles.question}`}>¿🏳️‍🌈?</span>
       <h2 className={`${styles.text} ${styles.question}`}>
         ¿Eres gei?
       </h2>
       <div className={styles.buttonsContainer}>
-        <button className={styles.button} onClick={() => setModalIsDisplayed(!modalIsDisplayed)}>Si 🏳️‍🌈</button>
-        <button className={styles.button}>Ño 🤮</button>
-      </div>      
-      {modalIsDisplayed && 
-        <ModalText 
-          content='jajaja re trolx sos. sabido era'
+        <button className={styles.button} onClick={() => setModalIsDisplayed(true)}>Si 🏳️‍🌈</button>
+        <button 
+          className={styles.button} 
+          onMouseOver={() => randomChangePosition(noBtnRef)} 
+          onClick={() => randomChangePosition(noBtnRef)} 
+          ref={noBtnRef}
+        >
+          Ño 🤮
+        </button>
+      </div>
+      {modalIsDisplayed &&
+        <ModalText
+          content='jajaja re trolx sos. sabido era, bellx'
+          buttonContent='Cerrar'
           close={() => setModalIsDisplayed(false)}
         />
       }
