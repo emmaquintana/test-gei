60c392e88dffb8f7f34596904003c4b3be6d4c07
Emmanuel Quintana <eqb2021@gmail.com>
2024-01-13 18:06:30 -0300

    fix: Fix commit with ID 5f7f5b6

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata, Viewport } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import Head from 'next/head'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -21,6 +22,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="es">
+      <Head>
+        <meta name="theme-color" content='#ff2890' />
+      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   )
